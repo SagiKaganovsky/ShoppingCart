3115441ffd6f5e8d6d538df2ad4470f6cdc0c2fe
Sergei Kaganovsky <sergkagan@gmail.com>
2020-01-28 15:56:35 +0200

    first commit

---
+++ ShoppingCart/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Nancy;
+using Nancy.Configuration;
+using Nancy.TinyIoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        //private readonly IAppConfiguration appConfig;
+
+        public Bootstrapper()
+        {
+        }
+
+        //public DemoBootstrapper(IAppConfiguration appConfig)
+        //{
+        //    this.appConfig = appConfig;
+        //}
+        public override void Configure(INancyEnvironment environment)
+        {
+            var config = new TraceConfiguration(enabled: false, displayErrorTraces: true);
+            environment.AddValue(config);
+        }
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            //container.Register<IAppConfiguration>(appConfig);
+        }
+    }
+}
---
+++ ShoppingCart/Modules/EventsFeedModule.cs
@@ -0,0 +1,28 @@
+﻿using Nancy;
+using ShoppingCart.EventFeed;
+
+namespace ShoppingCart.Modules
+{
+
+    public class EventsFeedModule : NancyModule
+    {
+        public EventsFeedModule(IEventStore eventStore) : base("/events")
+        {
+            Get("/", _ =>
+            {
+                long firstEventSequenceNumber, lastEventSequenceNumber;
+                if (!long.TryParse(this.Request.Query.start.Value, out firstEventSequenceNumber))
+                {
+                    firstEventSequenceNumber = 0;
+                }
+
+                if (!long.TryParse(this.Request.Query.end.Value, out lastEventSequenceNumber))
+                {
+                    lastEventSequenceNumber = long.MaxValue;
+                }
+
+                return eventStore.GetEvents(firstEventSequenceNumber, lastEventSequenceNumber);
+            });
+        }
+    }
+}
